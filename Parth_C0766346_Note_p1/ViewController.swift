1e65f2528d96cb4d30c0e762ac549db45497bf02
Parth Dalwadi <parthdalwadi@gmail.com>
2019-11-07 15:11:04 -0500

    added create folder functionality

--- Parth_C0766346_Note_p1/ViewController.swift
+++ Parth_C0766346_Note_p1/ViewController.swift
@@ -8,13 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
 
+    
+    @IBOutlet weak var folder_tableview: UITableView!
+    var folderNameList : [String]?
+    @IBOutlet weak var Edit_btn: UINavigationItem!
     override func viewDidLoad() {
         super.viewDidLoad()
+        folderNameList = []
         // Do any additional setup after loading the view.
+        Edit_btn.rightBarButtonItem = editButtonItem
+        
     }
 
-
+    @IBAction func Create_New_Folder(_ sender: UIBarButtonItem) {
+        
+        let makeFolderAlert = UIAlertController(title: "New Folder", message: "Enter a Name for this Folder", preferredStyle: .alert)
+        
+        makeFolderAlert.addTextField { (nameOfFolder) in
+            nameOfFolder.placeholder = "Name"
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+        }
+        
+        let addItem = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            let fName = makeFolderAlert.textFields![0].text
+            self.folderNameList?.append(fName!)
+            self.folder_tableview.reloadData()
+            
+            
+        }
+        
+        makeFolderAlert.addAction(cancel)
+        makeFolderAlert.addAction(addItem)
+        self.present(makeFolderAlert, animated: true, completion: nil)
+        
+        
+        
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return folderNameList?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "")
+        
+        cell.imageView?.image = UIImage(named: "folder-icon.png")
+        cell.textLabel?.text = folderNameList![indexPath.row]
+        
+        return cell
+        
+    }
+    
 }
 
